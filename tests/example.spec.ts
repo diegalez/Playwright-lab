566f509aaf30a85b73cffe916f05566305d05267
diegalez <dazb12@hotmail.com>
2024-07-12 13:48:16 -0500

    se sube proyecto

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,24 +6,6 @@ import { ScreenShot } from '../src/main/utils/ScreenShot';
 import store from 'store2'
 
 store('valor', '0');
-//const screenshot = new ScreenShot();
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
 
 test('get started global logic about us successful', async ({ page }, testInfo) => {
   
@@ -44,7 +26,6 @@ test('get started global logic about us successful', async ({ page }, testInfo) 
   const randomIndexx = Math.floor(Math.random() * allElementsHowdidyouhearaboutus.length);
   const randomOption = allElementsHowdidyouhearaboutus[randomIndexx];
   console.log('Opción seleccionada al azar__:', randomOption);
-  //await page.screenshot({path: 'screenshots/',fullPage:true});
   await screen.fullScreenShot(true);
 
 });
